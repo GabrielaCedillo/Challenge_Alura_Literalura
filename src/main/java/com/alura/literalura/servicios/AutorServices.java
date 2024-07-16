fc36a7c1e3ad0de27760971c6f50a29bcfc28e37
GabrielaCedillo <gabychubis.cf@gmail.com>
2024-07-16 12:50:11 -0600

    second commit from intellij

--- src/main/java/com/alura/literalura/servicios/AutorServices.java
+++ src/main/java/com/alura/literalura/servicios/AutorServices.java
@@ -1,4 +1,20 @@
 package com.alura.literalura.servicios;
 
+import com.alura.literalura.modelos.Autor;
+import com.alura.literalura.repositorio.IAutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
 public class AutorServices {
+    @Autowired
+    private IAutorRepository autorRepository;
+    public List<Autor> getAutoresVivosPorAno(int year){
+        return autorRepository.findAutorByFecha(year);
+    }
+    //Mandamos a llamar al metodo findAutorByFecha que busca y devuelve una lista de autores con fecha de defunción  posterior al año especificado y  fecha de nacimiento es anterior
+
 }
--- src/main/java/com/alura/literalura/servicios/ConsumoApi.java
+++ src/main/java/com/alura/literalura/servicios/ConsumoApi.java
@@ -1,4 +1,5 @@
 package com.alura.literalura.servicios;
 
+//Vamos a utilizar la Api proporcionada por alura, que es Gutendex, que nos deja buscar los libros que queramos
 public class ConsumoApi {
 }
